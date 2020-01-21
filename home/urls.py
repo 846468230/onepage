fd78c0f27a4fc416b98d4584b8c08703b88dc0f7
dao <846468230@qq.com>
2020-01-21 10:40:47 +0800

    项目已经完成

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,6 @@ app_name = "home"
 urlpatterns = [
     path('helloworld/',helloworld,name="helloworld"),
     path('homepage/',index,name="homepage"),
-    path('homepage/<int:user_id>',UserView.as_view(),name="page"),
+    path('homepage/<int:user_id>/',UserView.as_view(),name="page"),
     path('homepage/form/',message_post,name="form"),
 ]
